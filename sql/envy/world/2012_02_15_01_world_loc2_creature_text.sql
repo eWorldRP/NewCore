a87102cc7deaeeeaf2f1c30b1f597f999130cbaf
Envy602 <lionel.cuffel@sfr.fr>
2012-02-15 20:32:28 +0100

    DB\Loc2: Test Gundrak

---
+++ sql/envy/world/2012_02_15_01_world_loc2_creature_text.sql
@@ -0,0 +1,32 @@
+INSERT `locales_creature_text` (`entry`,`groupid`,`id`, `text_loc1`, `text_loc2`, `text_loc3`, `text_loc4`, `text_loc5`, `text_loc6`, `text_loc7`, `text_loc8`) VALUES
+
+-- GUNDRAK
+
+-- Slad'ran
+(29304,0,0,NULL,'Les Drakkari ils vont tuer tous sssceux qui empiètent sur sssces terres !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,1,0,NULL,'Tu ne respires plus ! Bien.',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,1,1,NULL,'Tu as encore peur ?',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,1,2,NULL,'Je te mange ensuite mec !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,2,0,NULL,'Je vois maintenant... Sssc''était pas le Fléau... Not''pire ennnemi...',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,3,0,NULL,'Sssserviteurs de l''écaille, répondez à mon appel !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29304,4,0,NULL,'Mille crocs assscérés vont lassscérer vos chairs !',NULL,NULL,NULL,NULL,NULL,NULL),
+-- Moorabi
+(29305,0,0,NULL,'Le Fléau on l''a repoussé. Tu crois que VOUS, vous avez une chance ?',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,1,0,NULL,'Je les écrase les cafards comme vous !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,1,1,NULL,'Qui c''est qui va m''arrêter ? Toi ?',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,1,2,NULL,'Plus si fort maintenant !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,2,0,NULL,'Si nos dieux ils peuvent mourir... Alors nous aussi...',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,3,0,NULL,'Préparez-vous à kek''chose de bien... plus... GRAND',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,4,0,NULL,'Le sol il va vous avaler d''un coup !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29305,5,0,NULL,'%s amorce sa transformation !',NULL,NULL,NULL,NULL,NULL,NULL),
+-- Gal'Darah
+(29306,0,0,NULL,'Tes tripes je vais les répandre, mec !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,1,0,NULL,'Quel pied !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,1,1,NULL,'Pas besoin de dieux, nous sommes des DIEUX !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,1,2,NULL,'Je vous l''avais dit !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,2,0,NULL,'Même les puissants... ils peuvent succomber.',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,3,0,NULL,'Étripez-les ! Empalez-les !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,3,1,NULL,'TUEZ-LES TOUS !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,3,2,NULL,'Dites bonjour à mon GRAND ami !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,4,0,NULL,'Après ça il restera plus rien !',NULL,NULL,NULL,NULL,NULL,NULL),
+(29306,5,0,NULL,'Tu veux voir la puissance ? Je vais te montrer la PUISSANCE !',NULL,NULL,NULL,NULL,NULL,NULL);
